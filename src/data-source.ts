332914340bffe9b39ebce7ab8764d2ac4118cd08
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-06-10 18:55:00 +0300

    setup database config

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,16 @@
+require('dotenv').config();
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { User } from "./entity/User"
 
-export const AppDataSource = new DataSource({
+export const BookStore: DataSource = new DataSource({
     type: "postgres",
     host: "localhost",
     port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectTimeoutMS: 5000,
     synchronize: true,
     logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+    entities: [`${__dirname}/entity/*.ts`]
+});
