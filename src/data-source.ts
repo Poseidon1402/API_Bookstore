02c4775949aabb4c2ddaa77cddf84f04f4c0b096
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-06-10 21:53:28 +0300

    code refactoring

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,16 +1,5 @@
-require('dotenv').config();
 import "reflect-metadata"
 import { DataSource } from "typeorm"
+import { dbconfig } from "./config/db.config";
 
-export const BookStore: DataSource = new DataSource({
-    type: "postgres",
-    host: process.env.DB_HOST,
-    port: 5432,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectTimeoutMS: 5000,
-    synchronize: true,
-    logging: false,
-    entities: [`${__dirname}/entity/*.ts`]
-});
+export const BookStore: DataSource = new DataSource(dbconfig);
