8d9c681f547bbb03648ce06597bd446e569256d7
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-06-10 22:04:07 +0300

    re-add db initialization

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,13 @@ const app: express.Express = express();
 const server: http.Server = http.createServer(app);
 const port: number | string = process.env.PORT || 4000;
 
+BookStore.initialize()
+    .then(() => {
+        console.log('Database intialized successfully !!');
+        return;
+    })
+    .catch(error => console.log(error))
+
 server.listen(port, () => {
     console.log(`The server is listening on port ${port}...`);
 });
