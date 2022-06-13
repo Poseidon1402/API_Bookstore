ecfc1a8c85fa882a5c978983045e47d89e1b17cf
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-06-13 19:52:49 +0300

    setup database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
+require('dotenv').config();
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DB_HOST,
+            port: 5432,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            entities: [],
+            synchronize: true,
+        })
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
