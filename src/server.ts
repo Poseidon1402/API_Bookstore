3fc6dc068840e8fb2cad5b196cc8479e03076d60
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-06-14 21:33:41 +0300

    use JSON as data formats

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import * as http from 'http';
 import * as express  from 'express';
 import { BookStore } from './data-source';
+import { bookRouter } from './routes/book.routes';
 
-const app: express.Express = express(); 
+const app: express.Express = express();
 const server: http.Server = http.createServer(app);
 const port: number | string = process.env.PORT || 4000;
+app.use(express.json()); 
 
-BookStore.initialize()
-    .then(() => {
-        console.log('Database intialized successfully !!');
-        return;
-    })
-    .catch(error => console.log(error))
+app.use('/books', bookRouter);
+
+(async () => {
+    await BookStore.initialize()
+});   
 
 server.listen(port, () => {
     console.log(`The server is listening on port ${port}...`);
