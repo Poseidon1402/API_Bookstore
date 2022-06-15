87299f7eb45b82a5185109a5dd085ee6c036a7ab
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-06-15 22:05:16 +0300

    establish an interface for the object that will be inserted inside the database

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -1,39 +1,50 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { DeleteResult, UpdateResult } from "typeorm";
 import { BookStore } from "../data-source";
+import { BookObjectForCreation, BookObjectForModification } from "../dto/book.dto";
 import { Book } from "../entity/Book.entity";
 
 export class BookOperation {
 
-    public static async retrieveAllBooks(req: Request, res: Response, next: NextFunction): Promise<Response> {
+    public static async retrieveAllBooks(req: Request, res: Response): Promise<Response> {
 
         const books: Book[] = await BookStore.manager.find(Book);
-
+        
         return res.status(200).json(books);
     }
 
-    public static async saveBook(req: Request, res: Response, next: NextFunction): Promise<Response> {
+    public static async saveBook(req: Request, res: Response): Promise<Response> {
 
-        await BookStore.manager.insert(Book, req.body);
+        try {
+            const book: BookObjectForCreation = req.body;
+    
+            await BookStore.manager.insert(Book, book);
+        }catch(err: any){
+            
+            return res.status(400).json({
+                Error: err
+            })
+        }
 
         return res.status(201).json({
             message: 'The book was saved successfully',
         });
     }
 
-    public static async modifyBookInfo(req: Request, res: Response, next: NextFunction): Promise<Response> {
+    public static async modifyBookInfo(req: Request, res: Response): Promise<Response> {
 
-        const book: UpdateResult = await BookStore.manager.update(Book, {book_number: req.params.id}, req.body);
+        const book: BookObjectForModification = req.body;
+        const result: UpdateResult = await BookStore.manager.update(Book, {book_number: req.params.id}, book);
 
         // when any ressource was updated
-        if(book.affected === 0) return res.status(404).json({ message: 'Ressource Not Found' });
+        if(result.affected === 0) return res.status(404).json({ message: 'Ressource Not Found' });
 
         return res.status(200).json({
             message: 'The ressource was updated successfully'
         })
     }
 
-    public static async removeBook(req: Request, res: Response, next: NextFunction): Promise<Response> {
+    public static async removeBook(req: Request, res: Response): Promise<Response> {
         
         const book: DeleteResult = await BookStore.manager.delete(Book, {
             book_number: req.params.id
