4a487b8b5e41df9490c24d87e5275c7104a98fb8
Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-07-26 20:40:44 +0300

    declare the db port as an environment variable

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -6,7 +6,7 @@ require('dotenv').config();
 export const dbconfig: any = {
     type: "postgres",
     host: process.env.DB_HOST,
-    port: 5432,
+    port: process.env.DB_PORT,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
