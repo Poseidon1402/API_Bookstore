55ceed7537abb4b28e2417ae21181387d93b3186
Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-07-28 22:34:42 +0300

    setup book's validation

--- src/entity/Book.entity.ts
+++ src/entity/Book.entity.ts
@@ -1,3 +1,4 @@
+import { IsInt, IsNotEmpty, IsNumber, Length } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
 
 @Entity()
@@ -7,20 +8,43 @@ export class Book {
     book_number: string;
 
     @Column({type: 'varchar', length: 30, nullable: false})
+    @Length(5, 30)
+    @IsNotEmpty()
     title: string;
 
     @Column({type: "varchar", length: 255, nullable: false})
+    @Length(5, 255)
+    @IsNotEmpty()
     description: string;
 
     @Column({type: "varchar", length: 25, nullable: false})
+    @Length(5, 25)
+    @IsNotEmpty()
     category: string;
 
     @Column({type: "int", nullable: false})
+    @IsInt()
+    @IsNotEmpty()
     price: number;
     
     @Column({type: "varchar", length: 15, nullable: false})
+    @IsNumber()
+    @IsNotEmpty()
     page_number: string;
 
     @Column({type: "varchar", length: 15, nullable: false})
+    @Length(2, 10)
+    @IsNotEmpty()
     language: string;
+
+    constructor(title: string, description: string, category: string,
+        price: number, page_number: string, language: string){
+
+            this.title = title;
+            this.description = description;
+            this.category = category;
+            this.price = price;
+            this.page_number = page_number;
+            this.language = language;
+    }
 }
---
+++ src/middlewares/bookRegistration.ts
@@ -0,0 +1,32 @@
+import { validate } from "class-validator";
+import { NextFunction, Request, Response } from "express";
+import { ValidationErrorInterface } from "../dto/utilities";
+import { Book } from "../entity/Book.entity";
+
+export class BookRegistration {
+
+    public static validateData(req: Request, res: Response, next: NextFunction){
+
+        const { title, desc, category, price, page_number, language} = req.body;
+        const book: Book = new Book(title, desc, category, price, page_number, language);
+
+        validate(book)
+            .then(errors => {
+                // errors is an array of validation errors
+                if (errors.length > 0) {
+                    
+                    let validatorFailed: ValidationErrorInterface[] = [];
+                    errors.forEach(err => {
+                        validatorFailed.push({
+                            property: err.property,
+                            message: err.constraints
+                        });
+                    });
+
+                    return res.status(400).json(validatorFailed);
+                }
+
+                next();
+            });
+    }
+}
