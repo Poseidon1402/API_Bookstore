728922798e65b9d115b1a446323b764c13bbc56f
Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-07-29 20:55:11 +0300

    setup the file uploader middleware

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,13 +1,15 @@
 import * as express from 'express';
 import { BookOperation } from '../controllers/book.controller';
 import { BookRegistration } from '../middlewares/bookRegistration';
+import { FileUploader } from '../middlewares/fileUploader';
 
 export const bookRouter: express.Router = express.Router();
 
 bookRouter.get('', BookOperation.retrieveAllBooks);
 bookRouter.post('', 
     [
-        BookRegistration.validateData
+        BookRegistration.validateData,
+        FileUploader.file.single('bookFile')
     ],
     BookOperation.saveBook
 );
