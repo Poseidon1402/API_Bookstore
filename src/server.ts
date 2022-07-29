ebd86b6f65be27a20556e8f86b574d09636aeb77
Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-07-29 21:37:19 +0300

    configure cors security

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,21 @@ import * as express  from 'express';
 import { BookStore } from './data-source';
 import { bookRouter } from './routes/book.routes';
 import { userRouter } from './routes/user.routes';
+import { NextFunction, Request, Response } from 'express';
 
 const app: express.Express = express();
 const server: http.Server = http.createServer(app);
 const port: number | string = process.env.PORT || 4000;
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 app.use(express.json());
-app.use(express.static(__dirname.split('src/')[0]+'public'));
+app.use(express.static(__dirname.split('/src')[0]+'/public'));
 
 // all base routes
 app.use('/books', bookRouter);
